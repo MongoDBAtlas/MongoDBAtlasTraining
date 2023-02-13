75cf03e047d6c273c810fb9fd2b5dc7f86351a84
Xers9 <xers989@gmail.com>
2023-02-14 01:02:23 +0900

    Index and aggregation

--- 03.index and aggregation/application/aggregation.js
+++ 03.index and aggregation/application/aggregation.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-//const uri = "mongodb+srv://<username>:<password>@***.***.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb+srv://admin:Rbehd1768@cluster0.pdvxq2k.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://<username>:<password>@***.***.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri);
 
