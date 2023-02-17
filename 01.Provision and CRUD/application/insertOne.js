221d9bce116b51dc1b091bad46a51586e3b145aa
Xers9 <xers989@gmail.com>
2023-02-18 02:28:51 +0900

    MQL

--- 01.Provision and CRUD/application/insertOne.js
+++ 01.Provision and CRUD/application/insertOne.js
@@ -9,15 +9,7 @@ async function run() {
       const database = client.db("handson");
       const userCollection = database.collection("user");
       // create a document to insert
-      const newUser = {
-        ssn:"123-456-0001", 
-        email:"user1@email.com", 
-        name:"Gildong Hong", 
-        DateOfBirth: "1st Jan.", 
-        Hobbies:["Martial arts"],
-        Addresses:[{"Address Name":"Work","Street":"431, Teheran-ro GangNam-gu ","City":"Seoul", "Zip":"06159"}], 
-        Phones:[{"type":"mobile","number":"010-5555-1234"}]
-      };
+      const newUser = <<query>>;
 
       const result = await userCollection.insertOne(newUser);
       console.log(`A document was inserted with the _id: ${result.insertedId}`);
